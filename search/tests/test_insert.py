c2259179767e10b30a7342f9b7a51f1404333d95
Terézia Slanináková <445526@mail.muni.cz>
2024-03-18 13:48:55 +0100

    Added missing imports

--- search/tests/test_insert.py
+++ search/tests/test_insert.py
@@ -8,6 +8,10 @@ import pandas as pd
 import numpy as np
 import h5py
 import os
+from pathlib import Path
+from urllib.request import urlretrieve
+import logging
+
 
 def get_data(data_part, **config):
     return np.array(
@@ -25,7 +29,7 @@ def get_data(data_part, **config):
 def download(src, dst):
     if not os.path.exists(dst):
         os.makedirs(Path(dst).parent, exist_ok=True)
-        LOG.info("downloading %s -> %s..." % (src, dst))
+        logging.info("downloading %s -> %s..." % (src, dst))
         urlretrieve(src, dst)
 
 
