b917b9e83c687d52bdea74fc6596373be2acb39f
Jan Mach <xmach3@fi.muni.cz>
2024-04-30 21:21:40 +0200

    two group by support

--- search/testing/graphs.py
+++ search/testing/graphs.py
@@ -1,10 +1,11 @@
 import os
 import re
 from dataclasses import dataclass
-from typing import List, Optional, Dict
+from typing import Dict, List, Optional
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 
 GRAPHS_DIR = "./graphs"
@@ -28,9 +29,9 @@ class Grapher:
     naive_pq: bool
     dynamic: bool
 
-    group_by_col_name: Optional[MetricName]
+    group_by_col_names: List[MetricName]
 
-    figsize = (10, 6)
+    figsize = (20, 8)
 
     def save_figure(self, figure: Figure, graph_type: str) -> None:
         filename = (
@@ -45,40 +46,75 @@ class Grapher:
         self,
         x_name: MetricName,
         y_name: MetricName,
-        group_by_name: Optional[MetricName] = None,
+        group_by_names: List[MetricName] = [],
         filter: Optional[Dict[str, List]] = None,
     ) -> Figure:
-        figure = plt.figure(figsize=self.figsize)
-
+        # FILTER
         filtered_data = self.data
         if filter is not None:
             for col, values in filter.items():
                 filtered_data = filtered_data[filtered_data[col].isin(values)]
 
-        if group_by_name is not None:
+        fig = plt.figure(figsize=self.figsize)
+
+        # GROUP BY
+        assert len(group_by_names) <= 2
+
+        if not group_by_names:
+            plt.scatter(filtered_data[x_name.col_name], filtered_data[y_name.col_name])
+
+            plt.xlabel(x_name.full_name)
+            plt.ylabel(y_name.full_name)
+            plt.legend()
+            plt.grid(True)
+
+        elif len(group_by_names) == 1:
             for group_by_value, group_by_data in filtered_data.groupby(
-                group_by_name.col_name
+                group_by_names[0].col_name
             ):
                 plt.scatter(
                     group_by_data[x_name.col_name],
                     group_by_data[y_name.col_name],
-                    label=f"{group_by_name.full_name} = {group_by_value}",
+                    label=f"{group_by_names[0].full_name} = {group_by_value}",
                 )
-        else:
-            plt.scatter(filtered_data[x_name.col_name], filtered_data[y_name.col_name])
 
-        plt.xlabel(x_name.full_name)
-        plt.ylabel(y_name.full_name)
-        plt.title(
+            plt.xlabel(x_name.full_name)
+            plt.ylabel(y_name.full_name)
+            plt.legend()
+            plt.grid(True)
+
+        elif len(group_by_names) == 2:
+            n_cols = len(filtered_data[group_by_names[1].col_name].unique())
+            fig, axs = plt.subplots(
+                1, n_cols, figsize=self.figsize, sharex=True, sharey=True
+            )
+            for (group_by_value, group_by_data), ax in zip(
+                filtered_data.groupby(group_by_names[1].col_name), axs
+            ):
+                assert isinstance(ax, Axes)
+                for group_by_value2, group_by_data2 in group_by_data.groupby(
+                    group_by_names[0].col_name
+                ):
+                    ax.scatter(
+                        group_by_data2[x_name.col_name],
+                        group_by_data2[y_name.col_name],
+                        label=f"{group_by_names[0].full_name} = {group_by_value2}",
+                    )
+
+                ax.set_xlabel(x_name.full_name)
+                ax.set_xlabel(y_name.full_name)
+                ax.set_title(f"{group_by_names[1].full_name} = {group_by_value}")
+                ax.legend()
+                ax.grid(True)
+
+        fig.suptitle(
             f"{x_name.full_name} vs. {y_name.full_name}\n"
             f"bucket type: {self.bucket_t}\n"
             f"{self.data_navigation}, {self.size}, k: {self.k}, "
             f"naive priority queue: {self.naive_pq}, dynamic search: {self.dynamic}"
         )
-        plt.legend()
-        plt.grid(True)
 
-        return figure
+        return fig
 
     def inference_time_to_recall(
         self, filter: Optional[Dict[str, List]] = None
@@ -86,7 +122,7 @@ class Grapher:
         return self._scatterplot(
             MetricName("inference", "Inference time"),
             MetricName("recall", "Recall"),
-            self.group_by_col_name,
+            self.group_by_col_names,
             filter,
         )
 
@@ -94,7 +130,7 @@ class Grapher:
         return self._scatterplot(
             MetricName("search", "Search time"),
             MetricName("recall", "Recall"),
-            self.group_by_col_name,
+            self.group_by_col_names,
             filter,
         )
 
@@ -104,7 +140,7 @@ class Grapher:
         return self._scatterplot(
             MetricName("distance_computations", "Distance computations"),
             MetricName("recall", "Recall"),
-            self.group_by_col_name,
+            self.group_by_col_names,
             filter,
         )
 
@@ -125,16 +161,18 @@ def get_grapher(filename: str) -> Grapher:
     path = os.path.join(TEST_RESULT_DIR, filename)
     data = pd.read_csv(path)
 
-    group_by_col_name = None
+    group_by_col_names = []
     if bucket_t == "naive":
-        group_by_col_name = None
+        group_by_col_names = []
     elif bucket_t in ["IVF", "IVFFaiss"]:
-        group_by_col_name = MetricName("nprobe", "nprobe")
+        group_by_col_names = [
+            MetricName("nprobe", "nprobe"),
+            MetricName("nlist", "nlist"),
+        ]
     elif bucket_t == "sketch":
-        group_by_col_name = MetricName("c", "c")
+        group_by_col_names = [MetricName("c", "c")]
     else:
         assert False
-    assert group_by_col_name is not None
 
     return Grapher(
         data=data,
@@ -145,7 +183,7 @@ def get_grapher(filename: str) -> Grapher:
         k=k,
         naive_pq=naive_pq,
         dynamic=dynamic,
-        group_by_col_name=group_by_col_name,
+        group_by_col_names=group_by_col_names,
     )
 
 
@@ -157,16 +195,13 @@ GRAPHS = [
     "search_time_to_recall",
     "distance_computations_to_recall",
 ]
-FILTER = {"nprobe": [1, 5, 9]}
+FILTER = {"nlist": [1, 5, 9, 15]}
 
 if __name__ == "__main__":
     for filename in FILENAMES:
         grapher = get_grapher(filename)
 
         for graph_type in GRAPHS:
-            try:
-                graph = getattr(grapher, graph_type)(FILTER)
-            except AttributeError:
-                continue
+            graph = getattr(grapher, graph_type)(FILTER)
 
             grapher.save_figure(graph, graph_type.replace("_", "-"))
