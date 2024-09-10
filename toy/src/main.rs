4c10962d6f9581d368ddf59b40143f71dcb80858
Marek <mareklicko448@gmail.com>
2024-09-10 12:56:18 +0200

    Working rust toy example with comparable recall + function for searching multiple buckets + simple parallel queries

---
+++ toy/src/main.rs
@@ -0,0 +1,377 @@
+#![allow(non_snake_case)]
+
+use std::collections::{HashMap, HashSet};
+use std::path::Path;
+
+use tch::data::Iter2;
+use tch::kind::Kind;
+use tch::nn::{self, Module, OptimizerConfig, Sequential};
+use tch::{no_grad, Device, IndexOp, Tensor};
+
+use ndarray::Array2;
+
+use kmeans::{KMeans, KMeansConfig};
+
+use rand::SeedableRng;
+
+use std::time::Instant;
+
+use rayon::prelude::*;
+
+const SEED: i64 = 42;
+
+struct LMI {
+    n_buckets: i64,
+    dimensionality: i64,
+    bucket_data: HashMap<i64, Tensor>,
+    bucket_data_ids: HashMap<i64, Tensor>,
+    model: Sequential,
+    epochs: i64,
+    lr: f64,
+    optimizer: nn::Optimizer,
+}
+
+impl LMI {
+    fn new(n_buckets: i64, data_dimensionality: i64, vs: &nn::VarStore) -> Self {
+        let path = &vs.root();
+
+        let model = nn::seq()
+            .add(nn::linear(
+                path,
+                data_dimensionality,
+                512,
+                Default::default(),
+            ))
+            .add_fn(|xs| xs.relu())
+            .add(nn::linear(path, 512, 384, Default::default()))
+            .add_fn(|xs| xs.relu())
+            .add(nn::linear(path, 384, n_buckets, Default::default()));
+
+        let lr = 0.001;
+        let optimizer = nn::Adam::default().build(vs, lr).unwrap();
+
+        LMI {
+            n_buckets,
+            dimensionality: data_dimensionality,
+            bucket_data: HashMap::new(),
+            bucket_data_ids: HashMap::new(),
+            model,
+            epochs: 10,
+            lr,
+            optimizer,
+        }
+    }
+
+    fn train(&mut self, X: &Tensor) {
+        assert_eq!(self.dimensionality, X.size()[1]);
+
+        // Run k-means to obtain training labels
+        println!("Running k-means...");
+        let now = Instant::now();
+        let v = Vec::<f32>::try_from(X.reshape([X.numel() as i64])).unwrap();
+        let kmeans: KMeans<_, 8> =
+            KMeans::new(v, X.size()[0] as usize, self.dimensionality as usize);
+        // Create seeded rng for reproducibility
+        let rnd = rand::rngs::SmallRng::seed_from_u64(SEED as u64);
+
+        // We want to change the rnd to our seeded rng
+        // We can't access the rnd field directly, so we need to create a new KMeansConfig with the seeded rng
+
+        let conf: KMeansConfig<f32> = KMeansConfig::build()
+            .iteration_done(&|s, nr, new_distsum| {
+                println!(
+                    "Iteration {} - Error: {:.2} -> {:.2} | Improvement: {:.2}",
+                    nr,
+                    s.distsum,
+                    new_distsum,
+                    s.distsum - new_distsum
+                )
+            })
+            .random_generator(rnd)
+            .build();
+
+        let kmeans = kmeans.kmeans_minibatch(
+            4096,
+            self.n_buckets as usize,
+            99999,
+            KMeans::init_random_sample,
+            &conf,
+        );
+        // let kmeans = kmeans.kmeans_lloyd(
+        //     self.n_buckets as usize,
+        //     15,
+        //     KMeans::init_random_sample,
+        //     // &KMeansConfig::default(),
+        //     &conf,
+        // );
+        println!("K-means finished in {:?}", now.elapsed());
+
+        // Prepare the data loader for training
+        // let dataset = LMIDataset::new(X.shallow_clone(), y);
+        let assignments = kmeans
+            .assignments
+            .iter()
+            .map(|&x| x as i64)
+            .collect::<Vec<i64>>();
+        let y: Tensor = Tensor::from_slice(&assignments);
+
+        println!("Training the model...");
+        let now = Instant::now();
+
+        let train_loader = Iter2::new(X, &y, 256).collect::<Vec<_>>();
+
+        // Train the model
+        for epoch in 1..=self.epochs {
+            for (X_batch, y_batch) in &train_loader {
+                let loss = self
+                    .model
+                    .forward(X_batch)
+                    .cross_entropy_for_logits(y_batch);
+                self.optimizer.backward_step(&loss);
+            }
+
+            println!(
+                "Epoch {} | Loss {:.5}",
+                epoch,
+                self.model
+                    .forward(X)
+                    .cross_entropy_for_logits(&y)
+                    .double_value(&[])
+            );
+        }
+
+        // Predict to which bucket each vector belongs
+        let classes = self.predict(X, 1).1.reshape([-1]);
+
+        // Store the vectors and their IDs in the corresponding buckets
+        for i in 0..self.n_buckets {
+            self.bucket_data.insert(
+                i,
+                X.index(&[Some(
+                    classes
+                        .eq_tensor(&Tensor::from(i as f64))
+                        .to_kind(Kind::Bool),
+                )]),
+            );
+            self.bucket_data_ids.insert(
+                i,
+                classes
+                    .eq_tensor(&Tensor::from(i as f64))
+                    .nonzero()
+                    .reshape([-1]),
+            );
+        }
+
+        println!("Training completed in {:?}", now.elapsed());
+    }
+
+    fn search(&self, query: &Tensor, k: i64) -> Tensor {
+        let bucket_id = self.predict(query, 1).1.int64_value(&[]);
+        let bucket_data = self.bucket_data.get(&bucket_id).unwrap();
+        let bucket_data_ids = self.bucket_data_ids.get(&bucket_id).unwrap();
+
+        let dists = (bucket_data - query)
+            .pow(&Tensor::from(2.0))
+            .sum_dim_intlist(1, false, Kind::Float)
+            .sqrt();
+
+        let indices = dists.sort(0, false).1.i((..k,));
+
+        bucket_data_ids.index(&[Some(indices)])
+    }
+
+    /// Search in multiple buckets and find closest k neighbors out of ALL of them
+    fn search_multiple_buckets(&self, query: &Tensor, k: i64, num_buckets: i64) -> Tensor {
+        let bucket_ids = self.predict(query, num_buckets).1;
+
+        let mut all_dists = Vec::new();
+        let mut all_data_ids = Vec::new();
+
+        // Loop over each predicted bucket
+        for i in 0..num_buckets {
+            let bucket_id = bucket_ids.int64_value(&[i]);
+            if let Some(bucket_data) = self.bucket_data.get(&bucket_id) {
+                if let Some(bucket_data_ids) = self.bucket_data_ids.get(&bucket_id) {
+                    // Calculate distances from the query to the items in this bucket
+                    let dists = (bucket_data - query)
+                        .pow(&Tensor::from(2.0))
+                        .sum_dim_intlist(1, false, Kind::Float)
+                        .sqrt();
+
+                    // Collect distances and corresponding data IDs
+                    all_dists.push(dists);
+                    all_data_ids.push(bucket_data_ids);
+                }
+            }
+        }
+
+        // Concatenate distances and data IDs from all buckets
+        let all_dists = Tensor::cat(&all_dists, 0);
+        let all_data_ids = Tensor::cat(&all_data_ids, 0);
+
+        // Sort all distances and get the top k closest ones
+        let indices = all_dists.sort(0, false).1.i((..k,));
+
+        // Return the data IDs corresponding to the top k closest items
+        all_data_ids.index(&[Some(indices)])
+    }
+
+    fn predict(&self, X: &Tensor, top_k: i64) -> (Tensor, Tensor) {
+        no_grad(|| {
+            let logits = self.model.forward(X);
+            logits.softmax(-1, Kind::Float).topk(top_k, -1, true, true)
+        })
+    }
+}
+
+fn main() {
+    println!("Starting...");
+    tch::manual_seed(SEED);
+
+    let now = Instant::now();
+    // Load the dataset
+    let dataset_path = Path::new("laion2B-en-clip768v2-n=100K.h5");
+    let file = hdf5::File::open(dataset_path).unwrap();
+    println!("Getting emb...");
+    let emb = file.dataset("emb").unwrap();
+
+    println!("Reading data...");
+    let data: Array2<f32> = emb.read_2d().unwrap();
+    println!("Slicing data...");
+    let X = Tensor::from_slice(data.as_slice().unwrap()).to_kind(Kind::Float);
+    println!("Reshaping data...");
+    let X = X.reshape([emb.shape()[0] as i64, emb.shape()[1] as i64]);
+
+    println!("Dataset loaded in {:?}", now.elapsed());
+
+    // let X = X.i((..1000, ..)); // ! LIMIT FOR TESTING
+    // println!("{:?}", X.size());
+
+    let (n, d) = X.size2().unwrap();
+
+    // Create an instance of the LMI
+    println!("Creating LMI...");
+    let vs = nn::VarStore::new(Device::cuda_if_available());
+    let mut lmi = LMI::new(320, d, &vs);
+
+    lmi.train(&X);
+
+    // Obtain a query from the user (random query from the dataset)
+    // println!("BRUH");
+    // let query = X.i((SEED, ..));
+    // println!("QUERY SIZE: {:?}", query.size());
+    // let k = 10;
+    // let nearest_neighbors = lmi.search(&query, k);
+
+    // // Evaluate the accuracy of the LMI's result
+    // let ground_truth = (X - query)
+    //     .pow(&Tensor::from(2.0))
+    //     .sum_dim_intlist(1, false, Kind::Float)
+    //     .sqrt();
+
+    // let (dists, indices) = ground_truth.sort(0, false);
+
+    // let topk = 10;
+    // let ground_truth_indices = indices.i((..topk,));
+    // let lmi_indices = nearest_neighbors.i((..topk,));
+
+    // println!("GROUND TRUTH INDICES (TOP 10): {}", ground_truth_indices);
+    // println!("LMI INDICES (TOP 10): {}", lmi_indices);
+
+    // let ground_truth_indices = Vec::<i64>::try_from(ground_truth_indices).unwrap();
+    // let ground_truth_indices: HashSet<i64, _> = HashSet::<i64>::from_iter(ground_truth_indices);
+
+    // let lmi_indices = Vec::<i64>::try_from(lmi_indices).unwrap();
+    // let lmi_indices: HashSet<i64, _> = HashSet::<i64>::from_iter(lmi_indices);
+
+    // let intersection = ground_truth_indices.intersection(&lmi_indices).count();
+
+    // println!("Intersection: {}", intersection);
+    // println!("Recall: {}", intersection as f64 / k as f64);
+
+    // Do the same thing as above, but with 100 queries (first 100 vectors in the dataset)
+    let n = 200;
+    let k: i64 = 10;
+    let mut recall_sum = 0.;
+
+    println!("Evaluating recall for first 200 queries...");
+    let now = Instant::now();
+
+    // for i in 0..n {
+    //     let query = X.i((i, ..)).squeeze();
+    //     // let nearest_neighbors = lmi.search(&query, k);
+    //     let nearest_neighbors = lmi.search_multiple_buckets(&query, k, 10);
+
+    //     let ground_truth = (&X - query)
+    //         .pow(&Tensor::from(2.0))
+    //         .sum_dim_intlist(1, false, Kind::Float)
+    //         .sqrt();
+
+    //     let (dists, indices) = ground_truth.sort(0, false);
+
+    //     let topk = 10;
+    //     let ground_truth_indices = indices.i((..topk,));
+    //     let lmi_indices = nearest_neighbors.i((..topk,));
+
+    //     let ground_truth_indices = Vec::<i64>::try_from(ground_truth_indices).unwrap();
+    //     if i == n - 1 {
+    //         println!("Ground truth: {:?}", ground_truth_indices);
+    //     }
+    //     let ground_truth_indices: HashSet<i64, _> = HashSet::<i64>::from_iter(ground_truth_indices);
+
+    //     let lmi_indices = Vec::<i64>::try_from(lmi_indices).unwrap();
+    //     if i == n - 1 {
+    //         println!("Predicted: {:?}", lmi_indices);
+    //     }
+    //     let lmi_indices: HashSet<i64, _> = HashSet::<i64>::from_iter(lmi_indices);
+
+    //     let intersection = ground_truth_indices.intersection(&lmi_indices).count();
+
+    //     recall_sum += intersection as f64 / k as f64;
+    // }
+
+    let X_ptr = &X as *const Tensor;
+    let X_raw_ptr = X_ptr as *const usize as usize;
+
+    let lmi_ptr = &lmi as *const LMI;
+    let lmi_raw_ptr = lmi_ptr as *const usize as usize;
+
+    let recall_sum: f64 = (0..n).into_par_iter().map(|i| {
+        let X = unsafe {&*(X_raw_ptr as *const Tensor)};
+        let lmi = unsafe {&*(lmi_raw_ptr as *const LMI)};
+        let query = X.i((i, ..)).squeeze();
+        let nearest_neighbors = lmi.search_multiple_buckets(&query, k, 10);
+    
+        let ground_truth = (X - query)
+            .pow(&Tensor::from(2.0))
+            .sum_dim_intlist(1, false, Kind::Float)
+            .sqrt();
+    
+        let (dists, indices) = ground_truth.sort(0, false);
+    
+        let topk = 10;
+        let ground_truth_indices = indices.i((..topk,));
+        let lmi_indices = nearest_neighbors.i((..topk,));
+    
+        let ground_truth_indices = Vec::<i64>::try_from(ground_truth_indices).unwrap();
+        let ground_truth_indices: HashSet<i64> = HashSet::from_iter(ground_truth_indices);
+    
+        let lmi_indices = Vec::<i64>::try_from(lmi_indices).unwrap();
+        let lmi_indices: HashSet<i64> = HashSet::from_iter(lmi_indices);
+    
+        let intersection = ground_truth_indices.intersection(&lmi_indices).count();
+    
+        intersection as f64 / k as f64
+    }).sum();
+
+    println!("Avg. Recall: {}", recall_sum / n as f64);
+    println!("Recall evaluated in {:?}", now.elapsed());
+
+    // let ground_truth = query
+    //     .dist(&X)
+    //     .argsort(-1, false)
+    //     .index(&[Some(0)])
+    //     .narrow(0, 0, k);
+    // let recall = nearest_neighbors.intersect1d(&ground_truth).size()[0] as f64 / k as f64;
+    // println!("Recall: {}", recall);
+}
---
+++ toy/src/original.py
@@ -0,0 +1,162 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import h5py
+import torch
+from faiss import Kmeans
+from torch import Tensor
+from torch.nn import CrossEntropyLoss, Linear, ReLU, Sequential
+from torch.nn.functional import softmax
+from torch.optim import Adam
+from torch.utils.data import DataLoader, Dataset
+
+SEED = 42
+torch.manual_seed(SEED)
+
+
+# Encapsulates the dataset and the labels for training
+class LMIDataset(Dataset):
+    def __init__(self, X: Tensor, y: Tensor):
+        self.X = X
+        self.y = y
+
+    def __len__(self) -> int:
+        return self.X.shape[0]
+
+    def __getitem__(self, index: int) -> tuple[Tensor, Tensor]:
+        return self.X[index], self.y[index]
+
+
+class LMI:
+    def __init__(self, n_buckets: int, data_dimensionality: int):
+        self.n_buckets: int = n_buckets
+        """Number of buckets."""
+        self.dimensionality: int = data_dimensionality
+        """Dimensionality of the data."""
+        self.bucket_data: dict[int, Tensor] = {}
+        """Mapping from bucket ID to the data in the bucket."""
+        self.bucket_data_ids: dict[int, Tensor] = {}
+        """Mapping from bucket ID to the indices of the data in the bucket."""
+
+        # Create a model
+        self.model = Sequential(
+            Linear(data_dimensionality, 512),
+            ReLU(),
+            Linear(512, 384),
+            ReLU(),
+            Linear(384, n_buckets),
+        )
+
+        # Model's hyperparameters
+        self.epochs = 10
+        self.lr = 0.001
+        self.loss_fn = CrossEntropyLoss()
+        self.optimizer = Adam(params=self.model.parameters(), lr=self.lr)
+
+    def train(self, X: Tensor) -> None:
+        assert self.dimensionality == X.shape[1]
+
+        # Run k-means to obtain training labels
+        kmeans = Kmeans(
+            d=self.dimensionality,
+            k=self.n_buckets,
+            verbose=True,
+            seed=SEED,
+        )
+        kmeans.train(X)
+        y = torch.from_numpy(kmeans.index.search(X, 1)[1].T[0])  # type: ignore
+
+        # Prepare the data loader for training
+        train_loader = DataLoader(
+            dataset=LMIDataset(X, y), batch_size=256, shuffle=True
+        )
+
+        # Train the model
+        self.model.train()
+
+        step = max(1, self.epochs // 10)
+        print(f"Epochs: {self.epochs}, step: {step}")
+
+        for epoch in range(self.epochs):
+            for X_batch, y_batch in train_loader:
+                loss = self.loss_fn(self.model(X_batch), y_batch)
+
+                # Do the backpropagation
+                loss.backward()
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+
+            if epoch % step == 0 and epoch != 0:
+                print(f"Epoch {epoch} | Loss {loss.item():.5f}")  # type: ignore
+
+        # Predict to which bucket each vector belongs
+        classes = self._predict(X, 1)[1].reshape(-1)
+
+        # Store the vectors and their IDs in the corresponding buckets
+        self.bucket_data = {i: X[classes == i] for i in range(self.n_buckets)}
+        self.bucket_data_ids = {
+            i: torch.where(classes == i)[0] for i in range(self.n_buckets)
+        }
+
+    def search(self, query: Tensor, k: int) -> Tensor:
+        # Bucket in which we should search for the nearest neighbors
+        bucket_id = int(self._predict(query, 1)[1].item())
+
+        # Retrieve the data and their IDs from the bucket
+        bucket_data = self.bucket_data[bucket_id]
+        bucket_data_ids = self.bucket_data_ids[bucket_id]
+
+        # Compute the k nearest neighbors in a brute-force way
+        # ! Beware: these are not global nearest neighbors IDs, but only the IDs of nearest neighbors within the bucket
+        nearest_neighbors = torch.cdist(query, bucket_data).topk(k, largest=False)[1][0]
+
+        # Convert the indices back to the global IDs
+        return bucket_data_ids[nearest_neighbors]
+
+    def _predict(self, X: Tensor, top_k: int) -> tuple[Tensor, Tensor]:
+        assert self.model is not None, "Model is not trained yet."
+
+        self.model.eval()
+
+        # Disable gradient computation
+        with torch.no_grad():
+            # Evaluate the model
+            logits = self.model(X)
+
+        # Compute probabilities from logits
+        probs = softmax(logits, dim=1)
+        # Select the top k most probable classes and their probabilities
+        probabilities, classes = probs.topk(top_k)
+
+        return probabilities, classes
+
+
+if __name__ == "__main__":
+    # Load the dataset
+    dataset_path = Path("laion2B-en-clip768v2-n=100K.h5")
+    X = torch.from_numpy(h5py.File(dataset_path, "r")["emb"][:]).to(torch.float32)  # type: ignore
+    n, d = X.shape
+
+    # Create an instance of the LMI
+    lmi = LMI(n_buckets=320, data_dimensionality=d)
+    lmi.train(X)
+
+    # Obtain a query from the user -- here we sample a random query from the dataset
+    query = X[torch.randint(0, n, (1,))]
+    # Number of neighbors to look for
+    k = 10
+    # Search for the k nearest neighbors
+    nearest_neighbors = lmi.search(query, k)
+
+    # Evaluate the accuracy of the LMI's result
+
+    # Calculate the ground truth for the query over the whole dataset
+    ground_truth = torch.argsort(torch.cdist(query, X)).reshape(-1)[:k]
+
+    # Calculate the recall -- closer to 1 is better
+    recall = (
+        len(set(nearest_neighbors.tolist()).intersection(set(ground_truth.tolist())))
+        / k
+    )
+    print(f"Recall: {recall}")
