cfc47849ccaf8429a03d5117579049a16af332a2
David Procházka <david@prochazka.dev>
2025-09-09 09:18:09 +0200

    Fixed typo

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ python task1.py --dataset-name imagenet-align-640-normalized &> result/log-image
 python task1.py --dataset-name laion-clip-512-normalized &> result/log-laion-clip-512-normalized.txt
 python task1.py --dataset-name llama-128-ip &> result/log-llama-128-ip.txt
 python task1.py --dataset-name yandex-200-cosine &> result/log-yandex-200-cosine.txt
-python task1.py --dataset-name yi-128-i &> result/log-yi-128-ip.txt
+python task1.py --dataset-name yi-128-ip &> result/log-yi-128-ip.txt
 
 echo "Done"
